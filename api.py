60d14e10ee1b45a3c7b9bf34bed4a781a9af9314
Brian Adams <bea@max.local>
2023-05-06 11:11:58 -0400

    first response!

--- api.py
+++ api.py
@@ -1,15 +1,19 @@
 import openai
 import pinecone
 from dotenv import load_dotenv
+import tiktoken
 import os
 
 load_dotenv()
 
 artists = os.getenv('ARTISTS').split(',')
+tke = tiktoken.get_encoding('cl100k_base')
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 openai.organization = os.getenv('OPENAI_ORGANIZATION_ID')
-openai_model = os.getenv('OPENAI_EMBEDDING_MODEL')
+openai_embedding_model = os.getenv('OPENAI_EMBEDDING_MODEL')
+openai_chat_model = os.getenv('OPENAI_CHAT_MODEL')
+token_limit = int(os.getenv('OPENAI_TOKEN_LIMIT'))
 
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
 pinecone_env = os.getenv('PINECONE_ENV') 
@@ -18,10 +22,52 @@ index_name = os.getenv('PINECONE_INDEX_NAME')
 pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
 index = pinecone.Index(index_name)
 
+def generate_prompt(text: str, context: list) -> str:
+    prompt = [] 
+    prompt.append('Context:')
+
+    global tke
+    global token_limit
+    token_count = len(tke.encode(prompt[0])) + len(tke.encode(text))
+
+    for ctxt in context:
+        token_count += len(tke.encode(ctxt))
+        if token_count >= token_limit:
+            break
+        else:
+            prompt.append(ctxt)
+    
+    prompt.append('Question:')
+    prompt.append(text)
+
+    pstr = '\n'.join(prompt)
+    print(pstr)
+    print(f'token count: {token_count}')
+
+    return pstr
+    
 def generate_embeddings(text: str) -> list:
-    global openai_model
-    response = openai.Embedding.create(model=openai_model, input=text)
+    global openai_embedding_model
+    response = openai.Embedding.create(model=openai_embedding_model, input=text)
     return response['data'][0]['embedding']
+
+def generate_completion(prompt: str) -> str:
+    global openai_chat_model
+    messages = [
+        { 
+            'role': 'system', 
+            'content': 'You are a philosopher and spiritual guide who provides amusing advice based on song lyrics included in this context:'
+        },
+        {
+            'role': 'user',
+            'content': prompt
+        }
+    ]
+    completion = openai.ChatCompletion.create(
+        model=openai_chat_model,
+        messages=messages
+    )
+    return completion.choices[0].message.content
 
 def upsert_embeddings(vectors: list, namespace: str):
     global index
--- chat.py
+++ chat.py
@@ -1,67 +1,32 @@
 import streamlit as st
 from streamlit_chat import message
-from api import generate_embeddings, query_db
+from api import generate_embeddings, query_db, generate_prompt, generate_completion
 import os
 
 # Setting page title and header
 st.set_page_config(page_title='SongChat', page_icon=':music:')
 st.markdown('<h1 style="text-align: center;">SongChat - a demo of ChatGPT with local data</h1>', unsafe_allow_html=True)
 
-# Initialise session state variables
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are conversationalist based on song lyrics."}
-    ]
-if 'model_name' not in st.session_state:
-    st.session_state['model_name'] = []
-
 st.sidebar.title('Sidebar')
-model_name = st.sidebar.radio('Choose a model:', ('GPT-3.5', 'GPT-4'))
-counter_placeholder = st.sidebar.empty()
 clear_button = st.sidebar.button('Clear Conversation', key='clear')
-
-# Map model names to OpenAI model IDs
-if model_name == 'GPT-3.5':
-    model = 'gpt-3.5-turbo'
-else:
-    model = 'gpt-4'
 
     # reset everything
 if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are conversationalist based on song lyrics."}
-    ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
+    pass
 
 # generate a response
 def generate_response(prompt: str):
-    st.session_state['messages'].append({'role': 'user', 'content': prompt})
-
-    #completion = openai.ChatCompletion.create(
-    #    model=model,
-    #    messages=st.session_state['messages']
-    #)
-    #response = completion.choices[0].message.content
-    #st.session_state['messages'].append({"role": "assistant", "content": response})
-
-    # print(st.session_state['messages'])
-    #total_tokens = completion.usage.total_tokens
-    #prompt_tokens = completion.usage.prompt_tokens
-    #completion_tokens = completion.usage.completion_tokens
-    #return response, total_tokens, prompt_tokens, completion_tokens
-
-def search(prompt: str):
+    
     embedding = generate_embeddings(prompt)
     results = query_db(embedding, ['wilco'])
+    context = []
+
     for match in results['matches']:
-        print(match['metadata']['text'])
+        context.append(match['metadata']['text'])
+
+    prompt = generate_prompt(prompt, context)
+    
+    return generate_completion(prompt)
 
 # container for chat history
 response_container = st.container()
@@ -74,30 +39,8 @@ with container:
         submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
-        search(user_input)
-        #output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
-        #st.session_state['past'].append(user_input)
-        #st.session_state['generated'].append(output)
-        #st.session_state['model_name'].append(model_name)
-        #st.session_state['total_tokens'].append(total_tokens)
-
-        # from https://openai.com/pricing#language-models
-        #if model_name == "GPT-3.5":
-        #    cost = total_tokens * 0.002 / 1000
-        #else:
-        #    cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
-
-        #st.session_state['cost'].append(cost)
-        #st.session_state['total_cost'] += cost
-
-#if st.session_state['generated']:
-#    with response_container:
-#        for i in range(len(st.session_state['generated'])):
-#            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-#            message(st.session_state["generated"][i], key=str(i))
-#            st.write(
-#                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
-#            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+        output = generate_response(user_input)
+        response_container.markdown(output)
 
 
 
